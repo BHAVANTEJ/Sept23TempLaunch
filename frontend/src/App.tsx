4152846296363a4dfe34193b066334488d71a021
Nandini N S <https://github.com/Nandinigowda99/Phase-01A.git>
2026-09-22 17:53:30 +0530

    HI I am Nandini

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,3 +4,4 @@ import { Countdown } from './components/Countdown';
 import { Footer } from './components/Footer';
 
 export default function App() { return <><Navbar /><main><Hero /><Countdown />{/* About / Our purpose content is intentionally disabled for this launch version. */}</main><Footer /></>; }
+export { App };
